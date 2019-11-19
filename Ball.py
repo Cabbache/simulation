69d4bbcd2af92bfe1f114e37864983c1c2a1ec2b
BuildTools <unconfigured@null.spigotmc.org>
2019-11-19 14:49:20 +0100

    v1

---
+++ Ball.py
@@ -0,0 +1,48 @@
+import math
+
+class Ball:
+	
+	def __init__(self,x=0,y=0,color=(255,255,255),size=10,vx=0,vy=0):
+		self.x = x
+		self.y = y
+		self.color = color
+		self.vx = vx
+		self.vy = vy
+		self.size = size
+	
+	def frame(self):
+		self.x += self.vx
+		self.y += self.vy
+	
+	def area(self):
+		return math.pi*pow(self.size,2)
+
+	def noRoot(self, ball):
+		return pow(ball.x-self.x,2) + pow(ball.y-self.y,2)
+
+	def distance(self, ball):
+		return math.sqrt(self.noRoot(ball))
+
+	def attract(self, body, G=1):
+		M = body.area()
+		m = self.area()
+		A = G*M/self.noRoot(body)
+		Ax = A/self.distance(body) * (body.x-self.x)
+		Ay = A/self.distance(body) * (body.y-self.y)
+		self.vx += Ax
+		self.vy += Ay
+
+	def getCoords(self):
+		return [int(self.x), int(self.y)]
+	
+	def isOverlap(self, mass):
+		return self.distance(mass) < self.size+mass.size
+	
+	def avg(self,x,y):
+		return (x+y)/2.0
+
+	def merge(self, ball):
+		mass = ball.area()+self.area()
+		newSize = int(math.sqrt(mass/math.pi))
+		#newX = ball.area()+
+		return Ball(self.avg(self.x,ball.x), self.avg(self.y,ball.y), size=newSize)
---
+++ billiard.py
@@ -0,0 +1,55 @@
+import pygame
+import math
+from random import randint
+from Ball import Ball
+
+G = 0.01
+pygame.init()
+
+white = (255,255,255)
+black = (0,0,0)
+
+yellow = (255,128,0)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((1200,800))
+gameDisplay.fill(black)
+pixAr = pygame.PixelArray(gameDisplay)
+w,h = pygame.display.get_surface().get_size()
+
+balls = []
+
+def solarSystem():
+	sun = 100
+	const = G*pow(sun,2)*math.pi
+	balls.append(Ball(600,400,red,size=sun))
+	balls.append(Ball(800,400,yellow,vy=math.sqrt(const/200.0)))
+	balls.append(Ball(300,400,yellow,vy=-math.sqrt(const/300.0)))
+	balls.append(Ball(1000,400,yellow,vy=-math.sqrt(const/400.0)))
+
+def random(times):
+	for x in xrange(times):
+		balls.append(Ball(randint(0,w), randint(0,h), white, size=randint(0,30)))
+
+#solarSystem()
+random(40)
+
+while True:
+	pygame.display.update()
+	pygame.event.get()
+	gameDisplay.fill(black)
+	for ball in balls:
+		ball.frame()
+		for other in balls:
+			if ball == other:
+				continue
+			if not(ball.isOverlap(other)):
+				other.attract(ball, G)
+			else:
+				balls.remove(ball)
+				balls.remove(other)
+				balls.append(ball.merge(other))
+				break
+		point = pygame.mouse.get_pos()
+		ball.attract(Ball(point[0], point[1], size=10), G)
+		pygame.draw.circle(gameDisplay, ball.color, ball.getCoords(), ball.size)
